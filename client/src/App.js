7fd108a2aae7c8254ad9e5d6f2dffa7ec585eee0
RicardoVP2002 <ricardo.vacape@gmail.com>
2022-01-21 19:35:40 -0500

    Creando Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import Box from "@mui/material/Box";
+import { Container, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      Prueba
-    </div>
+    <Container
+      maxWidth="sm"
+      fixed
+      style={{ textAlign: "center", height: "100%" }}
+    >
+      <img
+        src="https://logos-marcas.com/wp-content/uploads/2020/08/Bitcoin-Logo.png"
+        alt="Logo Bitcoin"
+        style={{ width: "250px", textAlign: "center", marginTop: "10px" }}
+      />
+      <Typography variant="h3" sx={{ mt: 2, mb: 2 }}>
+        Listado de criptomonedas{" "}
+      </Typography>
+      <div className="content">
+        <Box sx={{ flexGrow: 1, maxWidth: 752 }}></Box>
+      </div>
+    </Container>
   );
 }
 
